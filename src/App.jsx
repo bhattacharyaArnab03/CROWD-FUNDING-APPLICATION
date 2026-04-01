be14708a85b4f423a731e30bbeb2ef459f7dd6a2
Souptik Hazra <116510045+Souptik-Hazra@users.noreply.github.com>
2026-04-01 07:35:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import Admin from "./pages/Admin";
 import Explore from "./pages/Explore";
 import Payment from "./pages/Payment";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./Components/ProtectedRoute";
 import Home from "./pages/Home";
 
 function App() {
