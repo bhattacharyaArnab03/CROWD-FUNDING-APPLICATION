92015a50dad744347c46a85262db1240e6af10f9
Souptik Hazra <116510045+Souptik-Hazra@users.noreply.github.com>
2026-04-02 07:01:39 +0530

    Update generateTransactionNumber.js

--- microservices/payment-service/utils/generateTransactionNumber.js
+++ microservices/payment-service/utils/generateTransactionNumber.js
@@ -4,7 +4,7 @@ export function generateTransactionNumber(prefix = "TXN") {
   const dd = String(date.getDate()).padStart(2, '0');
   const mm = String(date.getMonth() + 1).padStart(2, '0');
   const yy = String(date.getFullYear()).slice(-2);
-  const randomNum = Math.floor(1000 + Math.random() * 9000); // 4-digit random number
+  const randomNum = Math.floor(10000000 + Math.random() * 90000000); // 8-digit random number to prevent JMeter collisions
   return `${prefix}-${dd}${mm}${yy}-${randomNum}`;
 }
 
